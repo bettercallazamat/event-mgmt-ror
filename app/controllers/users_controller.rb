ff06f477efba96b7a1f42647abc935a9888cd75a
bettercallazamat <bettercallazamat@gmail.com>
2020-05-26 01:03:31 +0600

    Generate User controller, model, routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Object successfully created"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
